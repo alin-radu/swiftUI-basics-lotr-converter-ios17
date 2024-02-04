9aebaa45b53d1bafc3c9161a9d078048b8a55f63
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-04 13:51:31 +0200

    Initial Commit

---
+++ LOTRConverter-ios17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter-ios17
+//
+//  Created by Alin RADU on 04.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter-ios17/LOTRConverter_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter_ios17App.swift
+//  LOTRConverter-ios17
+//
+//  Created by Alin RADU on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
